b2b521993c79667b2b72f2ef152c5404af5bdd38
Pankaj <Pankajsharma28292@gmail.com>
2024-10-19 02:21:57 +0530

    Pks

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+API_ID = int(getenv("API_ID", "24793246"))
+API_HASH = getenv("API_HASH", "4c19bc941eec1b863336d717dafc6f06")
+BOT_TOKEN = getenv("BOT_TOKEN", "8042134692:AAHaSbNp55qmpHng8z3FsjFut6iGU7gSCoM")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6172202347").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://pks_900:zmil7xlBYBaTBSca@cluster0.dm5qp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002393231571"))
